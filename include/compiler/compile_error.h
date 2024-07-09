e399e8361fc2af2a7aa6b1b86947b992fe40ee3f
drew-abbo <drew.a.abbo@gmail.com>
2024-07-09 16:49:08 -0400

    created compile error exceptions w/ some basic tests for the more complicated syntax error messages

---
+++ include/compiler/compile_error.h
@@ -0,0 +1,58 @@
+/// \file Holds the \p compile_error namespace which contains exceptions that
+/// can be thrown during compilation to stop the process.
+
+#ifndef COMPILE_ERROR_H
+#define COMPILE_ERROR_H
+
+#include <exception>
+#include <filesystem>
+#include <string>
+
+/// Contains exceptions that can be thrown during compilation to stop the
+/// process. All exceptions can be caught by catching \p compile_error::Generic
+/// which all other exceptions inherit from.
+///
+/// \p Generic (superclass)
+/// ├── \p CouldntOpenFile
+/// └── \p SyntaxError (superclass)
+///     └── \p BadToken
+///
+/// A newline is added to the end of all \p msg parameters.
+namespace compile_error {
+
+/// The root \p compile_error exception (shouldn't be thrown).
+class Generic : public std::exception {
+public:
+  explicit Generic(const std::string& msg);
+
+  /// Returns an error message.
+  virtual const char* what() const noexcept override;
+
+protected:
+  std::string m_msg;
+};
+
+/// Throw when an attempt to open a file fails.
+class CouldntOpenFile : public Generic {
+public:
+  explicit CouldntOpenFile(const std::filesystem::path& filePath);
+};
+
+/// The root exception for all syntax errors. Stores an index in a file and a
+/// file path for better debug info (shouldn't be thrown).
+class SyntaxError : public Generic {
+public:
+  explicit SyntaxError(const std::string& msg, const size_t indexInFile,
+                       const std::filesystem::path& filePath);
+};
+
+/// Throw when there is a problem tokenizing a file.
+class BadToken : public SyntaxError {
+public:
+  explicit BadToken(const std::string& msg, const size_t indexInFile,
+                    const std::filesystem::path& filePath);
+};
+
+} // namespace compile_error
+
+#endif // COMPILE_ERROR_H
---
+++ src/compiler/compile_error.cpp
@@ -0,0 +1,65 @@
+#include <compiler/compile_error.h>
+
+#include <filesystem>
+#include <fstream>
+#include <string>
+#include <cassert>
+
+using namespace compile_error;
+
+const char* Generic::what() const noexcept { return m_msg.c_str(); }
+
+Generic::Generic(const std::string& msg) : m_msg(msg + '\n') {}
+
+CouldntOpenFile::CouldntOpenFile(const std::filesystem::path& filePath)
+    : Generic("Failed to open file '" + filePath.string() + "'.") {}
+
+SyntaxError::SyntaxError(const std::string& msg, const size_t indexInFile,
+                         const std::filesystem::path& filePath)
+    : Generic(msg) {
+
+  m_msg += "File '" + filePath.string() + "', ";
+
+  std::ifstream file(filePath);
+
+  if (!file.is_open()) {
+  fileReadIssue:
+    // create an error message like this:
+    //    Unexpected character '@'.
+    //    File 'src/foo.mcfunc' at index 28 (failed to get more info).
+    m_msg += "index " + std::to_string(indexInFile) +
+             " (failed to get more info).\n";
+    return;
+  }
+
+  size_t i = 0, ln = 0, col = 0;
+
+  std::string line;
+  while (std::getline(file, line)) {
+    ln += 1;
+    const int lineLen = line.size() + 1;
+    if (i + lineLen > indexInFile) {
+      col = indexInFile - i + 1;
+      break;
+    }
+    i += lineLen;
+  }
+  if (col == 0) // never found
+    goto fileReadIssue;
+
+  // create an error message like this:
+  //    Unexpected character '@'.
+  //    File 'src/foo.mcfunc', ln 2, col 5:
+  //      foo(@);
+  //          ^ Here
+  m_msg += "ln " + std::to_string(ln) + ", col " + std::to_string(col) +
+           ":\n  " + line + "\n  ";
+  for (size_t j = 1; j < col; j++) {
+    m_msg += ' ';
+  }
+  m_msg += "^ Here\n";
+}
+
+BadToken::BadToken(const std::string& msg, const size_t indexInFile,
+                   const std::filesystem::path& filePath)
+    : SyntaxError(msg, indexInFile, filePath) {}
