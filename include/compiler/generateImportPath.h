a5a99993b1630a12c06c69e41d42660e781b4a85
drew-abbo <drew.a.abbo@gmail.com>
2024-07-23 02:18:31 -0400

    added missing header guards

--- include/compiler/generateImportPath.h
+++ include/compiler/generateImportPath.h
@@ -1,5 +1,8 @@
 /// \file Contains \p generateImportPath which can be used to make import paths.
 
+#ifndef GENERATEIMPORTPATH_H
+#define GENERATEIMPORTPATH_H
+
 #include <filesystem>
 
 /// Generates an import path by converting \param filePath and \param prefix to
@@ -7,3 +10,5 @@
 /// \param prefix can be an empty string.
 std::filesystem::path generateImportPath(const std::filesystem::path& filePath,
                                          const std::filesystem::path& prefix = "");
+
+#endif // GENERATEIMPORTPATH_H
