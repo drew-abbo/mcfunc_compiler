3a9bfdba4b16d4c8d439ea06f3154cb5aaadbb25
drew-abbo <drew.a.abbo@gmail.com>
2024-07-24 00:09:30 -0400

    polymorphism is a lot less weird with statements now

--- include/compiler/syntax_analysis/statement.h
+++ include/compiler/syntax_analysis/statement.h
@@ -5,6 +5,7 @@
 #define STATEMENT_H
 
 #include <cstddef>
+#include <memory>
 #include <vector>
 
 /// Contains classes that represent statements in code (like a function call or
@@ -18,10 +19,9 @@ namespace statement {
 
 /// The type that a statement is.
 enum class Kind {
-  GENERIC,
-  FUNCTION_CALL,  /// e.g. 'foo();'.
-  COMMAND,        /// e.g. '/say hi;' or '/execute as @a run: foo();'.
-  SCOPE,          /// e.g. '{ /say hi; }'.
+  FUNCTION_CALL, /// e.g. 'foo();'.
+  COMMAND,       /// e.g. '/say hi;' or '/execute as @a run: foo();'.
+  SCOPE,         /// e.g. '{ /say hi; }'.
 };
 
 /// The parent class for all other statements. \p Generic statement objects
@@ -33,13 +33,13 @@ protected:
   /// the semicolon).
   /// \param extraData A pointer to any extra data that is needs to be held.
   /// \warning Don't explicitly create instances of this, it's a base class.
-  Generic(size_t firstTokenIndex, size_t numTokens, void* extraData);
+  Generic(size_t firstTokenIndex, size_t numTokens);
 
 public:
   virtual ~Generic() = default;
 
   /// The kind of token this is.
-  virtual Kind kind() const;
+  virtual Kind kind() const = 0;
 
   /// The index of the first token.
   size_t firstTokenIndex() const;
@@ -50,7 +50,6 @@ public:
 protected:
   size_t m_firstTokenIndex;
   size_t m_numTokens;
-  void* m_extraData;
 };
 
 /// A function call as a statement.
@@ -65,7 +64,6 @@ public:
   /// The index of the token that holds the function's name.
   size_t functionNameTokenIndex() const;
 };
-static_assert(sizeof(FunctionCall) == sizeof(Generic));
 
 /// A command as a statement.
 class Command : public Generic {
@@ -76,12 +74,10 @@ public:
   /// \param firstTokenIndex The index of the first token.
   /// \param numTokens The number of tokens this statement takes up (including
   /// the semicolon and additional statement).
-  /// \param statementAfterRun A pointer to the statement that this command
-  /// runs after a 'run' argument (takes responsibility for freeing the
-  /// statement).
-  Command(size_t firstTokenIndex, size_t numTokens, Generic* statementAfterRun);
-
-  ~Command() override;
+  /// \param statementAfterRunPtr A pointer to the statement that this command
+  /// runs after a 'run' argument.
+  Command(size_t firstTokenIndex, size_t numTokens,
+          std::unique_ptr<Generic>&& statementAfterRunPtr);
 
   /// \p Kind::COMMAND is returned.
   Kind kind() const override;
@@ -93,29 +89,31 @@ public:
   bool hasStatementAfterRun() const;
 
   /// The statement this command runs after a 'run' argument.
-  const Generic& statementAfterRun() const;
+  const std::unique_ptr<Generic>& statementAfterRun() const;
+
+private:
+  std::unique_ptr<Generic> m_statementPtr;
 };
-static_assert(sizeof(Command) == sizeof(Generic));
 
 /// A scope as a statement.
 class Scope : public Generic {
 public:
-
   /// \param firstTokenIndex The index of the first token.
   /// \param numTokens The number of tokens this statement takes up.
   /// \param statements A vector of pointers to the statements that this scope
-  /// runs (takes ownership of the statements).
-  Scope(size_t firstTokenIndex, size_t numTokens, std::vector<Generic>* statements);
-
-  ~Scope() override;
+  /// runs.
+  Scope(size_t firstTokenIndex, size_t numTokens,
+        std::vector<std::unique_ptr<Generic>>&& statements);
 
   /// \p Kind::SCOPE is returned.
   Kind kind() const override;
 
   /// The statements that this scope runs.
-  const std::vector<Generic>& statements() const;
+  const std::vector<std::unique_ptr<Generic>>& statements() const;
+
+private:
+  std::vector<std::unique_ptr<Generic>> m_statements;
 };
-static_assert(sizeof(Scope) == sizeof(Generic));
 
 } // namespace statement
 
--- src/compiler/syntax_analysis/statement.cpp
+++ src/compiler/syntax_analysis/statement.cpp
@@ -8,13 +8,8 @@ using namespace statement;
 
 // Generic
 
-Generic::Generic(size_t firstTokenIndex, size_t numTokens, void* extraData)
-    : m_firstTokenIndex(firstTokenIndex), m_numTokens(numTokens), m_extraData(extraData) {}
-
-Kind Generic::kind() const {
-  assert(false && "'kind()' was called on a generic statement!");
-  return Kind::GENERIC;
-}
+Generic::Generic(size_t firstTokenIndex, size_t numTokens)
+    : m_firstTokenIndex(firstTokenIndex), m_numTokens(numTokens) {}
 
 size_t Generic::firstTokenIndex() const { return m_firstTokenIndex; }
 
@@ -22,7 +17,7 @@ size_t Generic::numTokens() const { return m_numTokens; }
 
 // FunctionCall
 
-FunctionCall::FunctionCall(size_t firstTokenIndex) : Generic(firstTokenIndex, 4, nullptr) {}
+FunctionCall::FunctionCall(size_t firstTokenIndex) : Generic(firstTokenIndex, 4) {}
 
 Kind FunctionCall::kind() const { return Kind::FUNCTION_CALL; }
 
@@ -30,37 +25,26 @@ size_t FunctionCall::functionNameTokenIndex() const { return 0; }
 
 // Command
 
-Command::Command(size_t firstTokenIndex) : Generic(firstTokenIndex, 2, nullptr) {}
+Command::Command(size_t firstTokenIndex) : Generic(firstTokenIndex, 2) {}
 
-Command::Command(size_t firstTokenIndex, size_t numTokens, Generic* statementAfterRun)
-    : Generic(firstTokenIndex, numTokens, statementAfterRun) {}
-
-Command::~Command() {
-  if (m_extraData != nullptr)
-    delete reinterpret_cast<Generic*>(m_extraData);
-}
+Command::Command(size_t firstTokenIndex, size_t numTokens,
+                 std::unique_ptr<Generic>&& statementAfterRunPtr)
+    : Generic(firstTokenIndex, numTokens), m_statementPtr(std::move(statementAfterRunPtr)) {}
 
 Kind Command::kind() const { return Kind::COMMAND; }
 
 size_t Command::commmandContentsTokenIndex() const { return 0; }
 
-bool Command::hasStatementAfterRun() const { return m_extraData != nullptr; }
+bool Command::hasStatementAfterRun() const { return m_statementPtr != nullptr; }
 
-const Generic& Command::statementAfterRun() const {
-  return *reinterpret_cast<Generic*>(m_extraData);
-}
+const std::unique_ptr<Generic>& Command::statementAfterRun() const { return m_statementPtr; }
 
 // Scope
 
-Scope::Scope(size_t firstTokenIndex, size_t numTokens, std::vector<Generic>* statements)
-    : Generic(firstTokenIndex, numTokens, statements) {}
-
-Scope::~Scope() {
-  delete reinterpret_cast<std::vector<Generic>*>(m_extraData);
-}
+Scope::Scope(size_t firstTokenIndex, size_t numTokens,
+             std::vector<std::unique_ptr<Generic>>&& statements)
+    : Generic(firstTokenIndex, numTokens), m_statements(std::move(statements)) {}
 
 Kind Scope::kind() const { return Kind::SCOPE; }
 
-const std::vector<Generic>& Scope::statements() const {
-  return *reinterpret_cast<std::vector<Generic>*>(m_extraData);
-}
+const std::vector<std::unique_ptr<Generic>>& Scope::statements() const { return m_statements; }
