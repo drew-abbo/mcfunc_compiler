11bdfa7e5dcab9da69bf5176b3fed8daf219fe23
drew-abbo <drew.a.abbo@gmail.com>
2024-10-18 21:47:28 -0400

    updated version

--- include/version.h
+++ include/version.h
@@ -1,7 +1,7 @@
 #pragma once
 /// \file Defines string macros that hold info about the build.
 
-#define MCFUNC_VERSION "0.1.0-beta.1"
+#define MCFUNC_VERSION "0.1.0-beta.2"
 #define MCFUNC_WEBSITE_LINK "https://github.com/drew-abbo/mcfunc_compiler"
 
 #define MCFUNC_BUILD_INFO_MSG "MCFunc v" MCFUNC_VERSION " " MCFUNC_WEBSITE_LINK
